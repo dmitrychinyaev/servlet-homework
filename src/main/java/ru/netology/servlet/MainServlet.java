0a31a9dae40eaf157d2513a237f03a082451d15f
Dmitry <dmitrychinyaev93@yandex.ru>
2023-01-24 15:49:58 +0300

    Добавил Spring framework зависимость в pom.xml. Конфигурация бинов через аннотации

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,6 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -21,9 +22,10 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        repository = new PostRepository();
-        service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        repository = context.getBean(PostRepository.class);
+        service = context.getBean(PostService.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
